aa19680255580a116e70b8dee25a80e924b481a3
Michael Hegner <hegner123@gmail.com>
2024-01-05 07:27:23 -0500

    updated tracking

---
+++ src/main/java/com/budget_planning/budget_planning_graphql/BudgetPlanningGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.budget_planning.budget_planning_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPlanningGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetPlanningGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budget_planning/budget_planning_graphql/BudgetPlanningGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget_planning.budget_planning_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlanningGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
